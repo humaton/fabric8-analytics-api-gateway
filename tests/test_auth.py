2631411833097d3886ca91dd1c6e44d8b7efd93b
Tomáš Hrčka <thrcka@redhat.com>
2018-05-31 14:41:06 +0200

    Auth test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,27 @@
 """Unit tests for the authorization module."""
+import json
+from unittest import TestCase
+from gateway import auth
+from gateway.api_v1 import app
 
-import unittest
+class TestAuth(TestCase):
+    """Test that endpoint is set from the right key:value pair."""
 
+    def setUp(self):
+        """Set it up."""
+        self.app = app.test_client()
 
-if __name__ == '__main__':
-    pass
+    def test_fetch_public_key(self):
+        """Test list of supported services."""
+        with self.assertRaises(Exception):
+            _=auth.fetch_public_key
+
+    def test_decode_token(self):
+        """Test list of supported services."""
+        with self.assertRaises(Exception):
+            _=auth.decode_token
+
+    def test_login_required(self):
+        """Test list of supported services."""
+        with self.assertRaises(Exception):
+            _=auth.decode_token
